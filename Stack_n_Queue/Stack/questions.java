d2ed103d284579cc11259cda086ec0f2c88ba711
Samyak Jain <samyaklearner@gmail.com>
2020-09-04 20:51:30 +0530

    update

--- Stack_n_Queue/Stack/questions.java
+++ Stack_n_Queue/Stack/questions.java
@@ -196,6 +196,29 @@ public String minRemoveToMakeValid(String str) {
 }
 
 
+public static int[] asteroidCollision(int[] arr) {
+    Stack<Integer> st = new Stack<>();
+    for (int i = 0; i < arr.length; i++) {
+        if(arr[i] > 0) st.push(arr[i]);
+        else{
+
+            while(st.size()!=0 && st.peek() > 0 && st.peek() < -arr[i]) st.pop();
+
+            if(st.size()!=0 && st.peek() == -arr[i]) st.pop();
+            else if(st.size() == 0 || st.peek() < 0 ) st.push(arr[i]);
+            else{
+                // astroid will distroy
+            }
+        }
+    }
+
+    int[] ans = new int[st.size()];
+    int i = st.size()-1;
+    while(st.size()!=0) ans[i--] = st.pop();
+
+    return ans;
+}
+
 
 
     

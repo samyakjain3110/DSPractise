214104dedd0602cef1f92ed33f7b5bdfd4c3c8e1
Samyak Jain <samyaklearner@gmail.com>
2020-09-04 20:52:22 +0530

    update

--- Stack_n_Queue/Stack/questions.java
+++ Stack_n_Queue/Stack/questions.java
@@ -258,6 +258,24 @@ public int largestRectangleArea(int[] heights) {
 }
     
 
+public int maximalRectangle(char[][] matrix) {
+    if( matrix.length == 0 ||  matrix[0].length == 0)  return 0;
+    int n = matrix.length;
+    int m = matrix[0].length;   
+
+    int[] heights = new int[m];
+    int area = 0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+           heights[j] = matrix[i][j] == '1' ? heights[j] + 1 : 0;
+        }
+
+        area = Math.max(area, largestRectangleArea(heights));
+    }
+    
+    return area;
+   }
+
 
 
 

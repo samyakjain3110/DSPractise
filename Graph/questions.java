95944bfef38b126434b3c35b4d0a895201ff83bd
Samyak Jain <samyaklearner@gmail.com>
2020-09-23 03:34:03 +0530

    update

--- Graph/questions.java
+++ Graph/questions.java
@@ -69,5 +69,30 @@ public class questions{
         return area;
     }
 
+    
+    //Leetcode 463
+    public int islandPerimeter(int[][] grid) {
+        if(grid.length==0 || grid[0].length==0) return 0;
+      
+      int n = grid.length;
+      int m = grid[0].length;
+      int count=0,nbrs=0;
+      for(int i=0;i<n;i++){
+          for(int j=0;j<m;j++){
+              if(grid[i][j] == 1){
+               count++;
+                for(int d=0;d<dir.length;d++){
+                  int r = i + dir[d][0];
+                  int c = j + dir[d][1];
+          
+                if(r>=0 && c>=0 && r<n && c<m&& grid[r][c]==1)
+                  nbrs++;
+              }
+            }
+        }
+      }
+      
+      return 4*count-nbrs; 
+    }
   
 }

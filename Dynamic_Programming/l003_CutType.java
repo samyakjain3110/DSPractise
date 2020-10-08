45723ed3811bf501a161ac8097aa69ba31474771
Samyak Jain <samyaklearner@gmail.com>
2020-10-08 23:55:36 +0530

    update

--- Dynamic_Programming/l003_CutType.java
+++ Dynamic_Programming/l003_CutType.java
@@ -527,6 +527,22 @@ public class l003_CutType{
         return ans; 
     }
 
+    // Leetcode 1216
+    public boolean isValidPalindrome(String s, int k) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        for(int gap = 0 ;gap <n;gap++){
+            for(int i =0,j=gap;j<n;i++,j++){
+                if(gap ==0) dp[i][j] = 1;
+                else if(gap == 1 && s.charAt(i) == s.charAt(j)) dp[i][j] = 2;
+                else if(s.charAt(i) == s.charAt(j)) dp[i][j] = dp[i+1][j-1] + 2;
+                else dp[i][j] = Math.max(dp[i+1][j],dp[i][j-1]);    
+            }
+        }
+        return n - dp[0][n-1] <= k;   
+    }
+
+
     public static void solve(){
         mcm();
     }

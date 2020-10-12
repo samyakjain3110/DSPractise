09e9414aaf5a83460fdc3ff6c6961d22c0d68cb2
Samyak Jain <samyaklearner@gmail.com>
2020-10-12 18:38:38 +0530

    update

--- Hashmap_n_Heap/l001questionHeap.java
+++ Hashmap_n_Heap/l001questionHeap.java
@@ -32,3 +32,29 @@ public class l001questionHeap{
         }
     }
     
+
+    //378
+public int kthSmallest(int[][] matrix, int k) {
+
+    int n = matrix.length;
+    int m = matrix[0].length;
+
+    PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->{
+        return matrix[a/m][a%m] - matrix[b/m][b%m];
+    });
+
+    for(int i=0;i<n;i++) pq.add(i * m + 0);
+
+    while(--k > 0){
+        int idx = pq.poll();
+        int r = idx / m;
+        int c = idx % m;
+
+        c++;
+        if(c < m) pq.add(r * m + c);
+    }
+
+    int val = pq.peek();
+    return Matrix[val/m][val%m];
+}
+

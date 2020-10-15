3fbc22e06453ce93743b6c34ee5572b1f927f357
Samyak Jain <samyaklearner@gmail.com>
2020-10-15 20:15:53 +0530

    update

--- Hashmap_n_Heap/ques_HashMap.java
+++ Hashmap_n_Heap/ques_HashMap.java
@@ -19,3 +19,24 @@ public class ques_HashMap{
         return ans;
     }
  
+     //350
+     public int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int ele: nums1) map.put(ele,map.getOrDefault(ele,0)+1);
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int ele: nums2){
+            if(map.containsKey(ele)){
+                map.put(ele,map.get(ele) - 1);
+                list.add(ele);
+                
+                if(map.get(ele) == 0) map.remove(ele);
+            }
+        }
+        
+        int[] ans = new int[list.size()];
+        for(int i=0;i<ans.length;i++) ans[i] = list.get(i);
+        
+        return ans;
+        
+    }

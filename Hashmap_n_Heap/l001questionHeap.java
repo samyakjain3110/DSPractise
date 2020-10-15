a87b8e22a15c431a05c09f71935aa520349770ec
Samyak Jain <samyaklearner@gmail.com>
2020-10-15 20:12:23 +0530

    update

--- Hashmap_n_Heap/l001questionHeap.java
+++ Hashmap_n_Heap/l001questionHeap.java
@@ -12,28 +12,28 @@ public class l001questionHeap{
         return pq.peek();
     }
 
-        // Leetcode 703
-        class KthLargest {
-            PriorityQueue<Integer> pq = new PriorityQueue<>();
-            int size = 0;
-            public KthLargest(int k, int[] nums) {
-                this.size = k;
-                for(int ele:nums){
-                    pq.add(ele);
-                    if(pq.size() > this.size) pq.poll();
-                }
-            }
-            
-            public int add(int val) {
-                pq.add(val);
+    // Leetcode 703
+    class KthLargest {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int size = 0;
+        public KthLargest(int k, int[] nums) {
+            this.size = k;
+            for(int ele:nums){
+                pq.add(ele);
                 if(pq.size() > this.size) pq.poll();
-                return pq.peek();
             }
         }
+        
+        public int add(int val) {
+            pq.add(val);
+            if(pq.size() > this.size) pq.poll();
+            return pq.peek();
+        }
     }
-    
+}
 
-    //378
+
+//378
 public int kthSmallest(int[][] matrix, int k) {
 
     int n = matrix.length;
@@ -57,7 +57,6 @@ public int kthSmallest(int[][] matrix, int k) {
     int val = pq.peek();
     return Matrix[val/m][val%m];
 }
-
 
 // 973
 public int[][] kClosest(int[][] points, int K) {
@@ -97,4 +96,27 @@ public static void nearlySorted(int[] arr,int k){
     }
 
     while(pq.size()!=0) arr[idx++] = pq.poll();
+}
+
+//356
+public boolean isReflected(int[][] points) {
+    HashSet<String> map = new HashSet<>();
+    int min = (int)1e9;
+    int max = -(int)1e9;
+    
+    for(int[] p : points){
+        max = Math.max(max,p[0]);
+        min = Math.min(min,p[0]);
+        
+        String str = p[0] + "@" + p[1];
+        map.add(str);
+    }
+    
+    int sum = max + min;
+    for(int[] p : points){
+        String str = (sum - p[0]) + "@" + p[1];
+        if(!map.contains(str)) return false;
+    }
+    
+    return true;   
 }

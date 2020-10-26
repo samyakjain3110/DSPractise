30c03fa76ad43cd74a1c7147c263d52993dc134c
Samyak Jain <samyaklearner@gmail.com>
2020-10-26 22:31:01 +0530

    update

--- Binary_Search/questions.java
+++ Binary_Search/questions.java
@@ -95,7 +95,26 @@ public class questions{
 
         return true;
     }
-
+    //Leetcode 33
+    public int search(int[] arr, int target) {
+        int lo = 0;
+        int hi = arr.length-1;
+        
+        while(lo <= hi){
+            int mid = (lo + hi ) >> 1;
+            if(arr[mid] == target) return mid;
+            else if(arr[lo] <= arr[mid]){
+                if(arr[lo] <= target && target < arr[mid]) hi = mid - 1;
+                else lo = mid + 1;
+            }else{
+                if(arr[mid] < target && target <= arr[hi]) lo = mid + 1;
+                else hi = mid - 1;
+            }
+        }
+        
+        return -1;
+    }
+ 
     
 
 }

0fe47c27758a4489fca0fbb62ec08335dc7a5c25
Samyak Jain <samyaklearner@gmail.com>
2020-10-26 22:22:43 +0530

    update

--- Binary_Search/questions.java
+++ Binary_Search/questions.java
@@ -17,3 +17,30 @@ public class questions{
         
         return false;
     }
+   // 658
+   public List<Integer> findClosestElements(int[] A, int k, int x) {
+    List<Integer> arr = new ArrayList<>();
+    for(int ele : A) arr.add(ele); 
+    
+    int n = A.length;
+    
+    if(x <= A[0]) return arr.subList(0,k);
+    else if(x >= A[n-1]) return arr.subList(n - k ,n);
+    else{
+        int idx  = Collections.binarySearch(arr,x);
+        if(idx < 0){
+            idx = -idx - 1; 
+        }
+        
+        int si = Math.max(0,idx - k);
+        int ei = Math.min(idx + k, n - 1);
+        
+        while(ei - si > k - 1){
+            if((x - A[si])  > (A[ei] - x)) si++;
+            else ei--;
+        }
+        return arr.subList(si,ei+1);
+    }   
+}
+
+}
